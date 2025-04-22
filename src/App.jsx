dc18e48a95c528f969947d32d1ef4cc3a36c44ad
Reiner Knudsen <rknudsen@conet.de>
2025-04-22 16:00:54 +0200

    installed firebase and setup sign-in route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.route";
 import Shop from "./routes/shop/shop.route";
+import SignIn from "./routes/sign-in/sign-in.route";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
