069f9416ee1d33177684fd6b523e2319e4c2c0a0
Reiner Knudsen <reinerknudsen@me.com>
2025-04-25 15:31:26 +0200

    created cart context && created cart list && toggle cart list

---
+++ src/components/card-icon/card-icon.component.jsx
@@ -0,0 +1,18 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+
+import { ReactComponent as ShoppingBag } from '../../assets/shopping-bag.svg';
+
+import './cart-icon.styles.scss';
+
+const CartIcon = () => {
+  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  return (
+    <div className='cart-icon-container' onClick={() => setIsCartOpen(!isCartOpen)}>
+      <ShoppingBag className='shopping-icon' />
+      <span className='item-count'>10</span>
+    </div>
+  );
+};
+
+export default CartIcon;
